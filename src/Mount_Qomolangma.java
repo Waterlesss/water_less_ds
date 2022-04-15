599bda22a004cc1bd9a82d018e9c6289123964fa
职回水 <1554333496@qq.com>
2022-04-15 23:28:20 +0800

    first test

---
+++ src/Mount_Qomolangma.java
@@ -0,0 +1,12 @@
+public class Mount_Qomolangma {
+    public static void main(String[] args) {
+        double mount = 884886;
+        double paper = 1.0;
+        int count = 0;
+        while (paper < mount) {
+            paper *= 2;
+            count ++;
+        }
+        System.out.println(count);
+    }
+}
