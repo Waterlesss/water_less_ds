803f11fb324e5bd4a22ba2923f884d6e01f59865
职回水 <1554333496@qq.com>
2022-04-24 23:35:41 +0800

    LeetCode NO.1 problem

---
+++ src/seqlist/leetcode/Code141.java
@@ -0,0 +1,29 @@
+package seqlist.leetcode;
+
+//public class Code141 {
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) {
+     *         val = x;
+     *         next = null;
+     *     }
+     * }
+//     */
+//    class Solution {
+//        public boolean hasCycle(ListNode head) {
+//            ListNode fast = head;
+//            ListNode low = head;
+//            while (fast != null && fast.next != null) {
+//                fast = fast.next.next;
+//                low = low.next;
+//                if (fast == low) {
+//                    return true;
+//                }
+//            }
+//            return false;
+//        }
+//    }
+//}
