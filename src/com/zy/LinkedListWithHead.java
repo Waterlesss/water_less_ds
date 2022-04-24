ad2e2474c73a6ab5aee88b593c6eea4190b4978d
职回水 <1554333496@qq.com>
2022-04-24 23:31:00 +0800

    LinkdeListWithHead

---
+++ src/com/zy/LinkedListWithHead.java
@@ -0,0 +1,87 @@
+package com.zy;
+
+public class LinkedListWithHead {
+    private int size;
+    private Node unrealHead = new Node();
+
+    public void add(int val) {
+        Node node = new Node(val, unrealHead.next);
+//        node.next = unrealHead.next;
+        unrealHead.next = node;
+        size++;
+    }
+
+    public void addIndex(int index, int val) {
+        if (index < 0 || index > size - 1) {
+            System.out.println("index is illegal ");
+            return;
+        }
+        Node prev = unrealHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+        Node node = new Node(val, prev.next);
+        prev.next = node;
+
+        size++;
+    }
+
+    public int remove(int index) {
+        if (index < 0 || index > size - 1) {
+            System.out.println("index is illegal ");
+            return -1;
+        }
+        Node prev = unrealHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+        Node node = prev.next;
+        int res = node.val;
+        prev.next = node.next;
+        node.next = null;
+        size --;
+        return res;
+    }
+
+    public void removeAllValue(int val) {
+        Node prev = unrealHead;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next= prev.next.next;
+                size--;
+            }else {
+                prev = prev.next;
+            }
+        }
+    }
+
+    class Node {
+        private int val;
+        private Node next;
+
+        public Node() {
+
+        }
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+
+
+    }
+    public String toString() {
+        String ret = "";
+        for (Node x = unrealHead.next;x != null;x = x.next) {
+            ret = ret + x.val;
+            ret = ret + "->";
+        }
+        ret = ret + "null";
+        return ret;
+    }
+
+}
---
+++ src/com/zy/WithHeadTest.java
@@ -0,0 +1,19 @@
+package com.zy;
+
+public class WithHeadTest {
+    public static void main(String[] args) {
+        LinkedListWithHead linkedListWithHead =new LinkedListWithHead();
+        linkedListWithHead.add(3);
+        linkedListWithHead.add(1);
+        linkedListWithHead.add(2);
+        linkedListWithHead.add(2);
+        linkedListWithHead.add(3);
+        linkedListWithHead.addIndex(0,3);
+        System.out.println(linkedListWithHead);
+//        linkedListWithHead.remove(0);
+//        System.out.println(linkedListWithHead.remove(0));
+        linkedListWithHead.removeAllValue(3);
+        System.out.println(linkedListWithHead);
+
+    }
+}
