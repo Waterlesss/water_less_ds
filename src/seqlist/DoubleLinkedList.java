d5037760aa12f209216eb50fbab9a07b740c05da
职回水 <1554333496@qq.com>
2022-04-26 13:08:17 +0800

    DoubleLinkedList add delete set get method

--- src/seqlist/DoubleLinkedList.java
+++ src/seqlist/DoubleLinkedList.java
@@ -1,16 +1,16 @@
 package seqlist;
 
 public class DoubleLinkedList {
-    private  int size;
+    private int size;
     //当前头结点
     private DoubleNode head;
     //当前尾结点
     private DoubleNode tail;
 
     public void addFist(int val) {
-        DoubleNode node = new DoubleNode(null,val,head);
+        DoubleNode node = new DoubleNode(null, val, head);
         if (head == null) {
-           tail = node;
+            tail = node;
         } else {
             head.prev = node;
         }
@@ -19,7 +19,7 @@ public class DoubleLinkedList {
     }
 
     public void addLast(int val) {
-        DoubleNode node = new DoubleNode(tail,val,null);
+        DoubleNode node = new DoubleNode(tail, val, null);
         if (tail == null) {
             head = node;
         } else {
@@ -29,7 +29,7 @@ public class DoubleLinkedList {
         size++;
     }
 
-    public void addIndex(int index,int val) {
+    public void addIndex(int index, int val) {
         if (index < 0 || index > size) {
             System.out.println("index is illegal");
             return;
@@ -40,11 +40,91 @@ public class DoubleLinkedList {
             addLast(val);
         } else {
             DoubleNode prev = findNode(index - 1);
-            DoubleNode node = new DoubleNode(prev,val,prev.next);
+            DoubleNode node = new DoubleNode(prev, val, prev.next);
             prev.next.prev = node;
             prev.next = node;
             size++;
         }
+    }
+
+    public void removeIndex(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("index is illegal");
+            return;
+        }
+        DoubleNode cur = findNode(index);
+        unlink(cur);
+    }
+
+    public void removeValueOnce(int val) {
+        for (DoubleNode x = head; x != null; x = x.next) {
+            if (x.val == val) {
+                unlink(x);
+                break;
+            }
+        }
+    }
+
+    public void removeAllValue(int val) {
+        for (DoubleNode x = head; x != null;) {
+            if (x.val == val) {
+                DoubleNode successor = x.next;
+                unlink(x);
+                x = successor;
+            } else {
+                x = x.next;
+            }
+        }
+    }
+
+    public void removeFirst() {
+        removeIndex(0);
+    }
+
+    public void removeLast() {
+        removeIndex(size - 1);
+    }
+
+    public void unlink(DoubleNode node) {
+        DoubleNode prev = node.prev;
+        DoubleNode successor = node.next;
+        if (prev == null) {
+            head = node.next;
+        } else {
+            prev.next = successor;
+            node.prev = null;
+        }
+        if (successor == null) {
+            tail = node.prev;
+        } else {
+            successor.prev = prev;
+            node.next = null;
+        }
+        size--;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("index is illegal");
+            return -1;
+        }
+        if (index == 0) {
+            return head.val;
+        }
+        DoubleNode prev = findNode(index - 1);
+        return prev.next.val;
+    }
+
+    public int set(int index, int newVal) {
+        if (index < 0 || index >= size) {
+            System.out.println("index is illegal");
+            return -1;
+        }
+        if (index == 0) {
+            return head.val = newVal;
+        }
+        DoubleNode prev = findNode(index - 1);
+        return prev.next.val = newVal;
     }
 
     private DoubleNode findNode(int index) {
@@ -53,10 +133,10 @@ public class DoubleLinkedList {
             for (int i = 0; i < index; i++) {
                 x = x.next;
             }
-            return  x;
+            return x;
         } else {
             DoubleNode x = tail;
-            for (int i =size - 1; i > index; i--) {
+            for (int i = size - 1; i > index; i--) {
                 x = x.prev;
             }
             return x;
@@ -65,7 +145,7 @@ public class DoubleLinkedList {
 
     public String toString() {
         String ret = "";
-        for(DoubleNode x = head; x != null;x = x.next) {
+        for (DoubleNode x = head; x != null; x = x.next) {
             ret = ret + x.val;
             ret = ret + "->";
         }
--- src/seqlist/DoubleLinkedTest.java
+++ src/seqlist/DoubleLinkedTest.java
@@ -6,13 +6,24 @@ public class DoubleLinkedTest {
 //        doubleLinkedList.addFist(1);
 //        doubleLinkedList.addFist(2);
 //        doubleLinkedList.addFist(3);
-        doubleLinkedList.addLast(1);
         doubleLinkedList.addLast(2);
-        doubleLinkedList.addLast(3);
+        doubleLinkedList.addLast(2);
+        doubleLinkedList.addLast(2);
+        doubleLinkedList.addLast(2);
+        doubleLinkedList.addLast(2);
+        doubleLinkedList.addLast(2);
         System.out.println(doubleLinkedList);
-        doubleLinkedList.addIndex(1,4);
-        doubleLinkedList.addIndex(1,5);
-        doubleLinkedList.addIndex(1,6);
+//        doubleLinkedList.set(2,4);
+//        doubleLinkedList.removeIndex(0);
+        doubleLinkedList.removeAllValue(2);
         System.out.println(doubleLinkedList);
+
+//        System.out.println(doubleLinkedList.get(0));
+//        System.out.println(doubleLinkedList.get(1));
+//        System.out.println(doubleLinkedList.get(3));
+//        doubleLinkedList.addIndex(1,4);
+//        doubleLinkedList.addIndex(1,5);
+//        doubleLinkedList.addIndex(1,6);
+//        System.out.println(doubleLinkedList);
     }
 }
